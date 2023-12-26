5464915a51bebc254d466f39afde43ef2fb4d8f4
nurselzengin <125567986+nurselzengin@users.noreply.github.com>
2023-12-26 22:17:35 +0300

    Transactions

---
+++ EasyCashIdentityProjectEntityLayer/Concrete/CustomerAccountTransactions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProjectEntityLayer.Concrete
+{
+    internal class CustomerAccountTransactions
+    {
+        public int CustomerAccountTransactionID { get; set; }
+        public string TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
